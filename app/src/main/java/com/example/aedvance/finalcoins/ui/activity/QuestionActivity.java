b78160cb979c218ddc02e4f367e6ffccd13a7777
王鑫 <wangxin@souche.com>
2017-05-08 21:13:21 +0800

    少传文件

---
+++ app/src/main/java/com/example/aedvance/finalcoins/ui/activity/QuestionActivity.java
@@ -0,0 +1,25 @@
+package com.example.aedvance.finalcoins.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.aedvance.finalcoins.R;
+
+/**
+ * <pre>
+ *     author : 王鑫
+ *     e-mail : wangxin@souche.com
+ *     time   : 2017/05/08
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class QuestionActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
